d315abaf40fbc8f5af56736ad10b83bc4f1503f1
신재혁 <shs0985@gmail.com>
2022-05-03 22:31:20 +0900

    페이스북, 구글 로그인 클릭 상태 추가

--- app/src/main/java/com/hci/obtt/ui/login/LoginFragment.kt
+++ app/src/main/java/com/hci/obtt/ui/login/LoginFragment.kt
@@ -1,20 +1,10 @@
 package com.hci.obtt.ui.login
 
-import android.os.Bundle
-import android.view.View
 import com.hci.obtt.R
 import com.hci.obtt.databinding.FragmentLoginBinding
 import com.hci.obtt.ui.BaseFragment
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonLogin.setOnClickListener {
-
-        }
-    }
 
     companion object {
 
