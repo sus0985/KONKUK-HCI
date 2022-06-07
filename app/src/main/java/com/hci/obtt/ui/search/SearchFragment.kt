723c2d720dae812b4a461ef15051ef85b25a05b9
serin301 <najin0250@gmail.com>
2022-06-08 00:53:52 +0900

    필터 적용 클릭 시 나오는 결과화면 구현

--- app/src/main/java/com/hci/obtt/ui/search/SearchFragment.kt
+++ app/src/main/java/com/hci/obtt/ui/search/SearchFragment.kt
@@ -1,9 +1,25 @@
 package com.hci.obtt.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import com.hci.obtt.R
 import com.hci.obtt.databinding.FragmentSearchBinding
 import com.hci.obtt.ui.base.BaseFragment
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_search_result, container, false)
+        val btn: Button = view.findViewById(R.id.filter_btn)
 
+        //nav 연결
+
+        return view
+    }
 }
