c2237506536b10aac6726f83be380c21bb9d400b
edd <edd@vlending.co.kr>
2022-06-07 10:03:44 +0900

    SearchResultFragment 추가

---
+++ app/src/main/java/com/hci/obtt/ui/search/SearchResultFragment.kt
@@ -0,0 +1,15 @@
+package com.hci.obtt.ui.search
+
+import com.hci.obtt.R
+import com.hci.obtt.databinding.FragmentSearchResultBinding
+import com.hci.obtt.ui.base.BaseFragment
+
+class SearchResultFragment : BaseFragment<FragmentSearchResultBinding>(R.layout.fragment_search_result) {
+
+    companion object {
+
+        fun newInstance() = SearchResultFragment()
+
+    }
+
+}
