7a5469a5c85ab33ef4c59b4470dbb8844a088822
serin301 <najin0250@gmail.com>
2022-06-12 23:14:57 +0900

    SearchFragment와 SearchResultfragment 연결해두었으나 오류 발생

--- app/src/main/java/com/hci/obtt/ui/search/SearchFragment.kt
+++ app/src/main/java/com/hci/obtt/ui/search/SearchFragment.kt
@@ -1,9 +1,20 @@
 package com.hci.obtt.ui.search
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.hci.obtt.R
 import com.hci.obtt.databinding.FragmentSearchBinding
 import com.hci.obtt.ui.base.BaseFragment
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        with(binding) {
+            filterBtn.setOnClickListener {
+                findNavController().navigate(R.id.action_searchFragment_to_searchResultFragment)
+            }
+        }
+    }
 }
